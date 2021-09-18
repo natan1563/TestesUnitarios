3494fadef64be2293171ce0b325445697a3c63d2
natan1563 <natromafe@gmail.com>
2021-09-18 17:32:14 -0300

    Teste unitario de data v2

--- app/Classes/Date.php
+++ app/Classes/Date.php
@@ -6,7 +6,50 @@ use Exception;
 
 class Date 
 {
-    public static function formatDate($date) {
+    private $date;
 
+    public function __construct($date) 
+    {
+        $date        = str_replace('-', '/', $date); 
+        $this->date  = explode('/', $date);
+    }
+
+    public function validateFormatDate() {
+        return count($this->date);
+    }
+
+    public function validateYearBisexto() 
+    {
+        $amountCharYear = strlen($this->date[2]);
+
+        return ($amountCharYear < 1 || $amountCharYear > 4);
+    }
+
+    // public function dayAndMonthValidate($date)
+    // {        
+    //     $bissexto    = (cal_days_in_month(CAL_GREGORIAN, 2, $dates[2]) == 29) ? 29 : 28;
+    //     $mapMonthDay = [
+    //         "01" => 31,
+    //         "02" => $bissexto,
+    //         "03" => 31,
+    //         "04" => 30,
+    //         "05" => 31,
+    //         "06" => 30,
+    //         "07" => 31,
+    //         "08" => 31,
+    //         "09" => 30,
+    //         "10" => 31,
+    //         "11" => 30,
+    //         "12" => 31
+    //     ];
+
+    //     if (!in_array($dates[1], array_keys($mapMonthDay))) return false;
+
+    //     return ($dates[0] <= $mapMonthDay[$dates[1]]);
+    // }
+
+    public function daysOfFebruaryHasTwentyEightDays() 
+    {
+        return (cal_days_in_month(CAL_GREGORIAN, 2, $this->date[2]) == 28);
     }
 }
--- app/Tests/DateTest.php
+++ app/Tests/DateTest.php
@@ -9,62 +9,100 @@ use PHPUnit\Framework\TestCase;
 class DateTest extends TestCase
 {
     
+    // /**
+    //  * @dataProvider dateProvider
+    //  */
+    // public function testShouldDateValid($date, $expected)
+    // {   
+
+    //     $actual = (!strlen($date)) ? false : $this->dayAndMonthValidate($date);
+        
+    //     $this->assertEquals($expected, $actual);
+    // }
+
     /**
-     * @dataProvider dateProvider
+     * @dataProvider dateSlashProvider
      */
-    public function testShouldDateValid($date, $expected)
-    {      
-        $actual = (!strlen($date)) ? false : $this->dayAndMonthValidate($date);
-        
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function dayAndMonthValidate($date)
+    public function testShouldHasTwoSlashes($date, $expected) 
     {
-        $date        = str_replace('-', '/', $date); 
-        $dates       = explode('/', $date);
-        
-        if (count($dates) != 3) return false;
-        
-        $bissexto    = (cal_days_in_month(CAL_GREGORIAN, 2, $dates[2]) == 29) ? 29 : 28;
-        $amountCharYear = strlen($dates[2]);
-
-        if ($amountCharYear < 1 || $amountCharYear > 4) return false;
-        
-        $mapMonthDay = [
-            "01" => 31,
-            "02" => $bissexto,
-            "03" => 31,
-            "04" => 30,
-            "05" => 31,
-            "06" => 30,
-            "07" => 31,
-            "08" => 31,
-            "09" => 30,
-            "10" => 31,
-            "11" => 30,
-            "12" => 31
-        ];
-
-        if (!in_array($dates[1], array_keys($mapMonthDay))) return false;
-
-        return ($dates[0] <= $mapMonthDay[$dates[1]]);
+        $date = new Date($date);
+        $this->assertEquals($expected, $date->validateFormatDate());
+    }
+    /**
+     * @dataProvider dateCountChars
+     */
+    public function testShouldHasTenDigits($date)
+    {
+        $this->assertEquals(10, strlen($date));
     }
 
-    public function dateProvider() {
+    /**
+     * @dataProvider daysOfFebruary
+     */
+    public function testDaysOfFebruaryShouldHasTwentyEightDays($date) 
+    {
+        $date = new Date($date);
+        $this->assertEquals(true, $date->daysOfFebruaryHasTwentyEightDays());
+    }
+ 
+
+    // public function dateProvider() {
+    //     return [
+    //         ['date' => '25/02/2001', "expected" => true],
+    //         ['date' => '23/02/2002', "expected" => true],
+    //         ['date' => '29/07/19999',"expected" => false],
+    //         ['date' => '',           "expected" => false],
+    //         ['date' => '29/02/2000',"expected"  => true],
+    //         ['date' => '29-01/2000',"expected"  => true],
+    //         ['date' => '29-01-2000',"expected"  => true],
+    //         ['date' => '29.01.2000',"expected"  => false],
+    //         ['date' => '29\\01\\2000',"expected"=> false],
+    //         ['date' => '00/01/2002',"expected"  => true],
+    //         ['date' => '00/',"expected"         => false],
+    //         ['date' => '// // //',"expected"    => false],
+    //     ];
+    // }
+
+    
+    public function dateCountChars() {
         return [
-            ['date' => '25/02/2001', "expected" => true],
-            ['date' => '23/02/2002', "expected" => true],
-            ['date' => '29/07/19999',"expected" => false],
-            ['date' => '',           "expected" => false],
-            ['date' => '29/02/2000',"expected"  => true],
-            ['date' => '29-01/2000',"expected"  => true],
-            ['date' => '29-01-2000',"expected"  => true],
-            ['date' => '29.01.2000',"expected"  => false],
-            ['date' => '29\\01\\2000',"expected"=> false],
-            ['date' => '00/01/2002',"expected"  => true],
-            ['date' => '00/',"expected"         => false],
-            ['date' => '// // //',"expected"    => false],
+            ['date' => '25/02/2001'],
+            ['date' => '23/02/2002'],
+            ['date' => '29/07/1999'],
+            ['date' => '29-02-2000'],
+            ['date' => '29-01/2000'],
+            ['date' => '29-01-2000'],
+            ['date' => '29.01.2000'],
+            ['date' => '00/01/2002'],
+        ];
+    }
+
+    public function daysOfFebruary() {
+        return [
+            ['date' => '25/02/2001'],
+            ['date' => '23/02/2002'],
+            ['date' => '29-02-2003'],
+            ['date' => '29-01/2009'],
+            ['date' => '29-01-2005'],
+            ['date' => '29-01-2001'],
+            ['date' => '00/01/2002'],
+        ];
+    }
+    
+    public function dateSlashProvider() {
+        return [
+            ['date' => '25/02/2001', "expected" => 3],
+            ['date' => '23/02/2002', "expected" => 3],
+            ['date' => '29/07.19999',"expected" => 2],
+            ['date' => '',           "expected" => 1],
+            ['date' => '29-02-2000', "expected"  => 3],
+            ['date' => '29-01/2000', "expected"  => 3],
+            ['date' => '29-01-2000', "expected"  => 3],
+            ['date' => '29.01.2000', "expected"  => 1],
+            ['date' => '29\\01\\2000', "expected"=> 1],
+            ['date' => '00/01/2002', "expected"  => 3],
+            ['date' => '00/', "expected"         => 2],
+            ['date' => '/ / /', "expected"       => 4],
         ];
     }
 }
